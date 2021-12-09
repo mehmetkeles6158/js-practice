e5cdf6ce5b5b9149a5a3608e7ce3d347de8d0bef
Mehmet <mehmetk6158@gmail.com>
2021-12-08 19:38:48 -0500

    all sums

--- js-practice2.js
+++ js-practice2.js
@@ -12,14 +12,14 @@ console.log(squareIt(5));
 
 function allSums (nums, k) {
   var count = 0;
-  for (var i = 0; i < nums.length-1; i++) {
-    for (var j = i + 1; j < nums.length; j++){
-      if (nums[i] + nums[j]== k) {
+  for (var i = 0; i < nums.length - 1; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === k) {
         count ++;
       }
     }
   }
-    console.log(count);
+  console.log(count);
 }
 
 allSums([1,2,3,4,5,6,7,8],7);
